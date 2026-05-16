0609fc169372884919a0e846a0ac2bb641424eaf
buh112q <lnwpoom.zaa123@gmail.com>
2026-05-16 23:14:05 +0700

    add keepassxc

--- modules/features/flatpak.nix
+++ modules/features/flatpak.nix
@@ -3,21 +3,22 @@
   inputs,
   ...
 }: {
-	flake.nixosModules.flatpak = {pkgs,...}: {
-		imports = [
-			inputs.nix-flatpak.nixosModules.nix-flatpak
-		];
-		services.flatpak = {
-			enable = true;
-			uninstallUnmanaged = true;
-			update.auto = {
-			  enable = true;
-			  onCalendar = "weekly"; # Default value
-			};
-			packages = [
-				"com.github.tchx84.Flatseal"
-				"it.mijorus.gearlever"
-			];
-		};
-	};
+  flake.nixosModules.flatpak = {pkgs, ...}: {
+    imports = [
+      inputs.nix-flatpak.nixosModules.nix-flatpak
+    ];
+    services.flatpak = {
+      enable = true;
+      uninstallUnmanaged = true;
+      update.auto = {
+        enable = true;
+        onCalendar = "weekly"; # Default value
+      };
+      packages = [
+        "com.github.tchx84.Flatseal"
+        "org.keepassxc.KeePassXC"
+        "it.mijorus.gearlever"
+      ];
+    };
+  };
 }
